161cd9b852a40697e27bd0292f6cfd0468797630
TheWerefriend <samtreeves@gmail.com>
2019-07-08 14:46:22 -0700

    Fixed URL, currencies.dict

--- scrape.py
+++ scrape.py
@@ -1,40 +1,64 @@
 #!/usr/bin/env python3
 
+# All written by Sam Reeves
+# s@mmk.global
+
+
+
 import time
-import requests
+from urllib.request import urlopen
 import datetime
 import pandas as pd
-import dill
+import json
+from os import path
+import ast
 
-# Define some useful variables. It appears that the db
-# starts in 2018.
-url_base = 'https://forex.cbm.gov/mm/api/history/'
-currencies = 'http://forex.cbm.gov.mm/api/currencies'
 date = pd.to_datetime('today')
+url_base = 'https://forex.cbm.gov.mm/api/history/'
 
+# Get and save the list of currencies
+if path.exists('currencies.dict'):
+    currencies = open('currencies.dict', 'r').read()
+    currencies = ast.literal_eval(currencies)
+else:
+    currencies  = urlopen('http://forex.cbm.gov.mm/api/currencies')
+    currencies = json.loads(currencies.read())
+    currencies = currencies['currencies']
+    f = open('currencies.dict', "w")
+    f.write(str(currencies))
+    f.close()
+
+
+#%%
 # Return a day's json from the CBM website
-def getDay(date):
-    response = requests.get(url_base + date.strftime('%d-%m-%Y'))
-    if response.content is None:
-        return [date, None]
-    else:
-        web_content = response.content
-        json_str = web_content.decode()
-        return [date, json_str]
 
-# Local data   [ { date : json_str }, {}, ...]
+def getDay(date):
+    response = urlopen(url_base + date.strftime('%d-%m-%Y'))
+    response = response.read()
+    data = json.loads(response)
+    data = data['rates']
+    return [data]
+
+
+# Local data   [ [date, dump], [], ... ]
 local_data = []
 null_count = 0
 
+#%%
 # Loop across the set, storing data
+
 while null_count <= 100:
-    local_data.insert(0, getDay(date))
+    day = {date : getDay(date)}
+    local_data.insert(0, day)
     date = date - datetime.timedelta(days=1)
-    if (local_data[1][1] and local_data[0][1]) == None:
+    
+    if local_data[1] and local_data[0] == []:
         null_count +=1
+        
     else:
         null_count = 0
     time.sleep(3)
 
+#%%
 # Write out the data
-dill.dump(local_data, open('local_data.p', "w"))
+    
